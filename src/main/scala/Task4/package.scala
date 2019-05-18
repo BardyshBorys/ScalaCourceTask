33ac3337a0463669f7aa65eaf97d64711d42e2a8
Borys <bardyshborys@gmail.com>
2019-05-18 17:16:27 +0300

    Add modifications

--- src/main/scala/Task4/package.scala
+++ src/main/scala/Task4/package.scala
@@ -44,52 +44,88 @@ package object Task4 {
 
   class UserRepositoryInMemoryId extends UserRepository[Id] {
     private var storage: Map[Long, User] = Map()
+    private var incremental_id: Long = 0
 
-    override def registerUser(username: String): Id[User] = ???
+    override def registerUser(username: String): Id[User] = {
+      incremental_id += 1
+      storage += (incremental_id -> User(incremental_id, username))
+      User(incremental_id, username)
+    }
 
-    override def getById(id: Long): Id[Option[User]] = ???
+    override def getById(id: Long): Id[Option[User]] = storage.get(id)
 
-    override def getByUsername(username: String): Id[Option[User]] = ???
+    override def getByUsername(username: String): Id[Option[User]] = {
+      storage.filter(_._2.username == username).values.toList.headOption
+    }
 
   }
 
   class UserRepositoryInMemoryFuture extends UserRepository[Future] {
 
     private var storage: Map[Long, User] = Map()
+    private var incremental_id: Long = 0
 
-    override def registerUser(username: String): Future[User] = ???
+    override def registerUser(username: String): Future[User] = Future.successful{
+      incremental_id += 1
+      storage += (incremental_id -> User(incremental_id, username))
+      User(incremental_id, username)
+    }
 
-    override def getById(id: Long): Future[Option[User]] = ???
+    override def getById(id: Long): Future[Option[User]] = Future.successful{
+      storage.get(id)
+    }
 
-    override def getByUsername(username: String): Future[Option[User]] = ???
+    override def getByUsername(username: String): Future[Option[User]] = Future.successful{
+      storage.filter(_._2.username == username).values.toList.headOption
+    }
 
   }
 
   class IotDeviceRepositoryInMemoryId extends IotDeviceRepository[Id] {
 
     private var storage: Map[Long, IotDevice] = Map()
+    private var incremental_id: Long = 0
 
-    override def registerDevice(userId: Long, serialNumber: String): Id[IotDevice] = ???
+    override def registerDevice(userId: Long, serialNumber: String): Id[IotDevice] = {
+      incremental_id += 1
+      storage += (incremental_id -> IotDevice(incremental_id, userId, serialNumber))
+      IotDevice(incremental_id, userId, serialNumber)
+    }
 
-    override def getById(id: Long): Id[Option[IotDevice]] = ???
+    override def getById(id: Long): Id[Option[IotDevice]] = storage.get(id)
 
-    override def getBySn(sn: String): Id[Option[IotDevice]] = ???
+    override def getBySn(sn: String): Id[Option[IotDevice]] = {
+      storage.filter(_._2.sn == sn).values.toList.headOption
+    }
 
-    override def getByUser(userId: Long): Id[Seq[IotDevice]] = ???
+    override def getByUser(userId: Long): Id[Seq[IotDevice]] = {
+      storage.filter(_._2.userId == userId).values.toSeq
+    }
 
   }
 
   class IotDeviceRepositoryInMemoryFuture extends IotDeviceRepository[Future] {
 
     private var storage: Map[Long, IotDevice] = Map()
+    private var incremental_id: Long = 0
 
-    override def registerDevice(userId: Long, serialNumber: String): Future[IotDevice] = ???
+    override def registerDevice(userId: Long, serialNumber: String): Future[IotDevice] = Future.successful{
+      incremental_id += 1
+      storage += (incremental_id -> IotDevice(incremental_id, userId, serialNumber))
+      IotDevice(incremental_id, userId, serialNumber)
+    }
 
-    override def getById(id: Long): Future[Option[IotDevice]] = ???
+    override def getById(id: Long): Future[Option[IotDevice]] = Future.successful{
+      storage.get(id)
+    }
 
-    override def getBySn(sn: String): Future[Option[IotDevice]] = ???
+    override def getBySn(sn: String): Future[Option[IotDevice]] = Future.successful{
+      storage.filter(_._2.sn == sn).values.toList.headOption
+    }
 
-    override def getByUser(userId: Long): Future[Seq[IotDevice]] = ???
+    override def getByUser(userId: Long): Future[Seq[IotDevice]] = Future.successful{
+      storage.filter(_._2.userId == userId).values.toSeq
+    }
 
   }
 
@@ -97,35 +133,35 @@ package object Task4 {
                          (implicit monad: Monad[F]) {
 
     def registerUser(username: String): F[Either[String, User]] = {
-      // .flatMap syntax works because of import cats.implicits._
-      // so flatMap function is added to F[_] through implicit conversions
-      // The implicit monad param knows how to flatmap and map over your F.
+
       repository.getByUsername(username).flatMap({
-        case Some(user) =>
-          monad.pure(Left(s"User $user already exists"))
-        case None =>
-          // .map syntax works because of import cats.implicits._
-          // so map function is added to F[_] through implicit conversions
-          repository.registerUser(username).map(Right(_))
+        case Some(user) => monad.pure(Left(s"User $user already exists"))
+        case None => repository.registerUser(username).map(Right(_))
       })
     }
 
-    def getByUsername(username: String): F[Option[String]] = ???
+    def getByUsername(username: String): F[Option[User]] = repository.getByUsername(username)
 
-    def getById(id: Long): F[Option[String]] = ???
+    def getById(id: Long): F[Option[User]] = repository.getById(id)
   }
 
   class IotDeviceService[F[_]](repository: IotDeviceRepository[F],
                                userRepository: UserRepository[F])
                               (implicit monad: Monad[F]) {
 
-    // the register should fail with Left if the user doesn't exist or the sn already exists.
-    def registerDevice(userId: Long, sn: String): F[Either[String, User]] = ???
+    def registerDevice(userId: Long, sn: String): F[Either[String, IotDevice]] = {
+      repository.getBySn(sn).flatMap({
+        case Some(_) => monad.pure(Left(s"Device Serial Number $sn already registered"))
+        case None => repository.registerDevice(userId, sn).map(Right(_))
+      })
+
+    }
+
+    def getById(id: Long): F[Option[IotDevice]] = repository.getById(id)
+
+    def getBySn(sn: String): F[Option[IotDevice]] = repository.getBySn(sn)
+
+    def getByUser(userId: Long): F[Seq[IotDevice]] = repository.getByUser(userId)
+
   }
-
-  // task1: implement in-memory Respository with Id monad.
-  // task2: implement in-memory Respository with Future monad
-  // example https://github.com/LvivScalaClub/cats-playground/blob/master/src/main/scala/BookRepository.scala
-
-  // task3: unit tests
 }
