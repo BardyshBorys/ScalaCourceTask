0d646fd6e0e3fda483f3aeecedd03b72287d592e
Borys <bardyshborys@gmail.com>
2019-05-19 14:17:09 +0300

    Create Unit Tests

--- src/main/scala/Task4/package.scala
+++ src/main/scala/Task4/package.scala
@@ -1,23 +1,14 @@
+import cats.{Id, Monad}
+
 import scala.concurrent.Future
 
 package object Task4 {
 
-  import cats.{Id, Monad}
-
-  /**
-    * Repository and Service implementation using tagless final pattern.
-    * The idea is to make it easier to test our database layer, using Scala’s higher kinded types to abstract
-    * the Future type constructor away from our traits under test.
-    * Intro to tagless final: https://www.basementcrowd.com/2019/01/17/an-introduction-to-tagless-final-in-scala/.
-    * The similar task example https://github.com/LvivScalaClub/cats-playground/blob/master/src/main/scala/BookRepository.scala
-    */
 
   case class User(id: Long, username: String)
 
   case class IotDevice(id: Long, userId: Long, sn: String)
 
-  // NOTE: This import bring into the scope implicits that allow you to call .map and .flatMap on the type F[_]
-  // and also bring you typeclasses that know how to flatmap (Monad) and map (Functor) over your higher-kinded type.
   import cats.implicits._
 
   trait UserRepository[F[_]] {
@@ -65,17 +56,17 @@ package object Task4 {
     private var storage: Map[Long, User] = Map()
     private var incremental_id: Long = 0
 
-    override def registerUser(username: String): Future[User] = Future.successful{
+    override def registerUser(username: String): Future[User] = Future.successful {
       incremental_id += 1
       storage += (incremental_id -> User(incremental_id, username))
       User(incremental_id, username)
     }
 
-    override def getById(id: Long): Future[Option[User]] = Future.successful{
+    override def getById(id: Long): Future[Option[User]] = Future.successful {
       storage.get(id)
     }
 
-    override def getByUsername(username: String): Future[Option[User]] = Future.successful{
+    override def getByUsername(username: String): Future[Option[User]] = Future.successful {
       storage.filter(_._2.username == username).values.toList.headOption
     }
 
@@ -109,21 +100,21 @@ package object Task4 {
     private var storage: Map[Long, IotDevice] = Map()
     private var incremental_id: Long = 0
 
-    override def registerDevice(userId: Long, serialNumber: String): Future[IotDevice] = Future.successful{
+    override def registerDevice(userId: Long, serialNumber: String): Future[IotDevice] = Future.successful {
       incremental_id += 1
       storage += (incremental_id -> IotDevice(incremental_id, userId, serialNumber))
       IotDevice(incremental_id, userId, serialNumber)
     }
 
-    override def getById(id: Long): Future[Option[IotDevice]] = Future.successful{
+    override def getById(id: Long): Future[Option[IotDevice]] = Future.successful {
       storage.get(id)
     }
 
-    override def getBySn(sn: String): Future[Option[IotDevice]] = Future.successful{
+    override def getBySn(sn: String): Future[Option[IotDevice]] = Future.successful {
       storage.filter(_._2.sn == sn).values.toList.headOption
     }
 
-    override def getByUser(userId: Long): Future[Seq[IotDevice]] = Future.successful{
+    override def getByUser(userId: Long): Future[Seq[IotDevice]] = Future.successful {
       storage.filter(_._2.userId == userId).values.toSeq
     }
 
@@ -164,4 +155,5 @@ package object Task4 {
     def getByUser(userId: Long): F[Seq[IotDevice]] = repository.getByUser(userId)
 
   }
+
 }
--- src/test/scala/Task4Test.scala
+++ src/test/scala/Task4Test.scala
@@ -1,26 +1,148 @@
-import Task3.Validator._
-import Task3._
+import Task4._
+import cats.Id
+import cats.instances.future._
 import org.scalatest.FunSuite
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
+import scala.language.higherKinds
+
 
 class Task4Test extends FunSuite {
 
-  test("Task3.validator") {
-    assert((2 validate (lessThan(10) and positiveInt)).isRight)
-    assert((-1 validate (positiveInt and lessThan(10))).isLeft)
-    assert((-1 validate (lessThan(10) or positiveInt)).isRight)
+  test("Task4.tagless") {
+    val userRepository = new UserRepositoryInMemoryId()
+    val iotDeviceRepository = new IotDeviceRepositoryInMemoryId()
 
-    assert(("" validate nonEmpty).isRight)
-    assert(("Foo" validate nonEmpty).isLeft)
-    assert((Person(name = "John", age = 25) validate isPersonValid).isRight)
-    assert((Person(name = "John", age = 125) validate isPersonValid).isLeft)
+    val userService = new UserService[Id](userRepository)
+    val iotDeviceService = new IotDeviceService[Id](iotDeviceRepository, userRepository)
 
-    assert(234.validate.isRight)
-    assert(("asdasd" validate).isRight)
-    assert((Person(name = "John", age = 25) validate).isRight)
+    val userName1 = "Ash Ketchum"
+    val userName2 = "Brock"
+    val userName3 = "Misty"
 
-    assert((2 validate (lessThan(10) and positiveInt and lessThan(3))).isRight)
-    assert((-1 validate ((positiveInt and lessThan(10)) or lessThan(1))).isRight)
-    assert((-1 validate (lessThan(10) or positiveInt)).isRight)
+    userService.registerUser(userName1)
+    userService.registerUser(userName2)
+    userService.registerUser(userName3)
+
+
+    val user1 = userService.getByUsername(userName1)
+    val user2 = userService.getByUsername(userName2)
+    val user3 = userService.getByUsername(userName3)
+
+    assert(user1.get.id == 1)
+    assert(user2.get.id == 2)
+    assert(user3.get.id == 3)
+
+    assert(user1.get.username == userName1)
+    assert(user2.get.username == userName2)
+    assert(user3.get.username == userName3)
+
+
+    val deviceData1 = (user1.get.id, "Pikachu")
+    val deviceData2 = (user2.get.id, "Onix")
+    val deviceData3 = (user3.get.id, "Hypno's Naptime")
+
+    (iotDeviceService.registerDevice _).tupled(deviceData1)
+    (iotDeviceService.registerDevice _).tupled(deviceData2)
+    (iotDeviceService.registerDevice _).tupled(deviceData3)
+
+    assert(iotDeviceService.getByUser(user1.get.id).headOption.get.id == 1)
+    assert(iotDeviceService.getByUser(user2.get.id).headOption.get.id == 2)
+    assert(iotDeviceService.getByUser(user3.get.id).headOption.get.id == 3)
+
+    assert(iotDeviceService.getByUser(user1.get.id).headOption.get.sn == deviceData1._2)
+    assert(iotDeviceService.getByUser(user2.get.id).headOption.get.sn != deviceData1._2)
+    assert(iotDeviceService.getByUser(user3.get.id).headOption.get.sn != deviceData1._2)
+
+    assert(iotDeviceService.getByUser(user1.get.id).headOption.get.sn != deviceData2._2)
+    assert(iotDeviceService.getByUser(user2.get.id).headOption.get.sn == deviceData2._2)
+    assert(iotDeviceService.getByUser(user3.get.id).headOption.get.sn != deviceData2._2)
+
+    assert(iotDeviceService.getByUser(user1.get.id).headOption.get.sn != deviceData3._2)
+    assert(iotDeviceService.getByUser(user2.get.id).headOption.get.sn != deviceData3._2)
+    assert(iotDeviceService.getByUser(user3.get.id).headOption.get.sn == deviceData3._2)
+
+    assert(iotDeviceService.getByUser(user1.get.id).headOption.get.userId == user1.get.id)
+    assert(iotDeviceService.getByUser(user1.get.id).headOption.get.userId != user2.get.id)
+
+    assert(iotDeviceService.getByUser(user2.get.id).headOption.get.userId == user2.get.id)
+    assert(iotDeviceService.getByUser(user2.get.id).headOption.get.userId != user3.get.id)
+
+    assert(iotDeviceService.getByUser(user3.get.id).headOption.get.userId == user3.get.id)
+    assert(iotDeviceService.getByUser(user3.get.id).headOption.get.userId != user1.get.id)
+
+    assert(iotDeviceService.getBySn(deviceData1._2).headOption.get.userId == user1.get.id)
+    assert(iotDeviceService.getBySn(deviceData2._2).headOption.get.userId == user2.get.id)
+    assert(iotDeviceService.getBySn(deviceData3._2).headOption.get.userId == user3.get.id)
+
+    assert(iotDeviceService.getBySn(deviceData1._2).headOption.get.sn == deviceData1._2)
+    assert(iotDeviceService.getBySn(deviceData2._2).headOption.get.sn == deviceData2._2)
+    assert(iotDeviceService.getBySn(deviceData3._2).headOption.get.sn == deviceData3._2)
+
+  }
+
+  test("Task4.taglessFuture") {
+    def await[T](f: Future[T]) = Await.result(f, 2.seconds)
+
+
+    val userRepository = new UserRepositoryInMemoryFuture()
+    val iotDeviceRepository = new IotDeviceRepositoryInMemoryFuture()
+
+    val userService = new UserService[Future](userRepository)
+    val iotDeviceService = new IotDeviceService[Future](iotDeviceRepository, userRepository)
+
+    val userName1 = "Ash Ketchum"
+    val userName2 = "Brock"
+    val userName3 = "Misty"
+
+    await(userService.registerUser(userName1))
+    await(userService.registerUser(userName2))
+    await(userService.registerUser(userName3))
+
+
+    val user1 = await(userService.getByUsername(userName1))
+    val user2 = await(userService.getByUsername(userName2))
+    val user3 = await(userService.getByUsername(userName3))
+
+    assert(user1.get.id == 1)
+    assert(user2.get.id == 2)
+    assert(user3.get.id == 3)
+
+    assert(user1.get.username == userName1)
+    assert(user2.get.username == userName2)
+    assert(user3.get.username == userName3)
+
+
+    val deviceData1 = (user1.get.id, "Pikachu")
+    val deviceData2 = (user2.get.id, "Onix")
+    val deviceData3 = (user3.get.id, "Hypno's Naptime")
+
+    (iotDeviceService.registerDevice _).tupled(deviceData1)
+    (iotDeviceService.registerDevice _).tupled(deviceData2)
+    (iotDeviceService.registerDevice _).tupled(deviceData3)
+
+    assert(await(iotDeviceService.getByUser(user1.get.id)).headOption.get.id == 1)
+    assert(await(iotDeviceService.getByUser(user2.get.id)).headOption.get.id == 2)
+    assert(await(iotDeviceService.getByUser(user3.get.id)).headOption.get.id == 3)
+
+    assert(await(iotDeviceService.getByUser(user1.get.id)).headOption.get.sn == deviceData1._2)
+    assert(await(iotDeviceService.getByUser(user2.get.id)).headOption.get.sn != deviceData1._2)
+    assert(await(iotDeviceService.getByUser(user3.get.id)).headOption.get.sn != deviceData1._2)
+    assert(await(iotDeviceService.getByUser(user1.get.id)).headOption.get.sn != deviceData2._2)
+    assert(await(iotDeviceService.getByUser(user2.get.id)).headOption.get.sn == deviceData2._2)
+    assert(await(iotDeviceService.getByUser(user3.get.id)).headOption.get.sn != deviceData2._2)
+    assert(await(iotDeviceService.getByUser(user1.get.id)).headOption.get.sn != deviceData3._2)
+    assert(await(iotDeviceService.getByUser(user2.get.id)).headOption.get.sn != deviceData3._2)
+    assert(await(iotDeviceService.getByUser(user3.get.id)).headOption.get.sn == deviceData3._2)
+
+    assert(await(iotDeviceService.getByUser(user1.get.id)).headOption.get.userId == user1.get.id)
+    assert(await(iotDeviceService.getByUser(user1.get.id)).headOption.get.userId != user2.get.id)
+    assert(await(iotDeviceService.getByUser(user2.get.id)).headOption.get.userId == user2.get.id)
+    assert(await(iotDeviceService.getByUser(user2.get.id)).headOption.get.userId != user3.get.id)
+    assert(await(iotDeviceService.getByUser(user3.get.id)).headOption.get.userId == user3.get.id)
+    assert(await(iotDeviceService.getByUser(user3.get.id)).headOption.get.userId != user1.get.id)
 
   }
 }
